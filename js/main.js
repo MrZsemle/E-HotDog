4e37d962be326c4a6df403c02a68ff058dfbb6ce
MrZsemle <zsombi1022@gmail.com>
2019-04-30 21:16:44 +0200

    click animation

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     var click = 0;
     var i = 1;
+
     $(".snackbar").hide();
     $(".hotdog").hover(function () {
             setTimeout(function () {
@@ -14,13 +15,28 @@ $(document).ready(function () {
     );
     var timeoutId = 0;
 
+    $(".hotdog").hover(function () {
+        $(this).removeClass("ar");
+        $(this).addClass("a");
+            
+        }, function () {
+            $(this).removeClass("a");
+            $(this).addClass("ar");
+            $(this).removeClass("acr");
+        }
+    );
+
     $('.hotdog').on('mousedown', function () {
         timeoutId = setTimeout(1000);
         var click = i++;
         console.log(click);
         $(".click").text("Click: "+click);
+        $(this).removeClass("acr");
+        $(this).addClass("ac");
         
     }).on('mouseup mouseleave', function () {
         clearTimeout(timeoutId);
+        $(this).removeClass("ac");
+        $(this).addClass("acr");
     });
 });
