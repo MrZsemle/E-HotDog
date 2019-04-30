872092051c67fcde598c68ec4491e7f4af58b7aa
MrZsemle <zsombi1022@gmail.com>
2019-04-30 19:36:46 +0200

    counter

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    var click = 0;
+    var i = 1;
     $(".snackbar").hide();
     $(".hotdog").hover(function () {
             setTimeout(function () {
@@ -10,4 +12,15 @@ $(document).ready(function () {
             }, 1500); 
         }
     );
+    var timeoutId = 0;
+
+    $('.hotdog').on('mousedown', function () {
+        timeoutId = setTimeout(1000);
+        var click = i++;
+        console.log(click);
+        $(".click").text("Click: "+click);
+        
+    }).on('mouseup mouseleave', function () {
+        clearTimeout(timeoutId);
+    });
 });
